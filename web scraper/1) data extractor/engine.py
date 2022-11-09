e1b5c7d7bb382571de11d1ef4308914859da1c12
Nickita <fattahovnikita@mail.ru>
2022-11-09 19:52:36 +0500

    begin of new branch

--- web scraper/1) data extractor/engine.py
+++ web scraper/1) data extractor/engine.py
@@ -10,14 +10,25 @@ response = requests.get(url)
 
 html = response.text
 
-#file = open('info.html', 'w+', encoding="utf-8")
+#file = open('info.html', 'w', encoding="utf-8")
 #file.write(html)
+with open("info.html",encoding="utf-8") as file:
+	file = file.read()
 
 soup = BeautifulSoup(file, "lxml")
-print(soup.html)
-all_cinemas_hrefs = soup.find_all(class_="cinemaList_ref")
 
+all_cinemas_hrefs = soup.find_all(class_="cinemaList_ref")
+all_cinemas_names = soup.find_all(class_="cinemaList_name")
+all_cinemas_addresses = soup.find_all(class_="cinemaList_addr")
 for item in all_cinemas_hrefs:
-	print(item)
+	item_href = item.get("href")
+	print(item_href)
+for item in all_cinemas_names:
+	item_name = item.text
+	print(item_name)
+for item in all_cinemas_addresses:
+	item_address = item.text
+	print(item_address)
+
 
 #file.close()
