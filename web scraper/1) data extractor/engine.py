819b7467fe6bcb01c39407f9194b59860069ed8b
Nickita <fattahovnikita@mail.ru>
2022-11-09 20:13:32 +0500

    Add links, addresses, names

--- web scraper/1) data extractor/engine.py
+++ web scraper/1) data extractor/engine.py
@@ -16,7 +16,17 @@ with open("info.html", encoding="utf-8") as file:
     file = file.read()
 
 soup = BeautifulSoup(file, "lxml")
+
 all_cinemas_hrefs = soup.find_all(class_="cinemaList_ref")
+all_cinemas_names = soup.find_all(class_="cinemaList_name")
+all_cinemas_addresses = soup.find_all(class_="cinemaList_addr")
 
 for item in all_cinemas_hrefs:
-    print(item)
+	item_href = item.get("href")
+	print(item_href)
+for item in all_cinemas_names:
+	item_name = item.text
+	print(item_name)
+for item in all_cinemas_addresses:
+	item_address = item.text
+	print(item_address)
