1d22f721e216b78a1900402fdf75ccb6ba27c9f7
BezukhVladimir <vladimir.bezukh@yandex.ru>
2022-11-09 22:54:16 +0500

    analysis, optimization, decomposition and refactoring; add cinema.py, class Cinema; add lib/file_checks.py.

---
+++ web scraper/1) data extractor/cinema.py
@@ -0,0 +1,6 @@
+class Cinema:
+
+    def __init__(self, href, name, addresses):
+        self.href = href
+        self.name = name
+        self.addresses = addresses
--- web scraper/1) data extractor/engine.py
+++ web scraper/1) data extractor/engine.py
@@ -1,32 +1,39 @@
-import os.path
-
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://perm.kinoafisha.info/cinema/"
+from cinema import Cinema
+from lib.file_checks import does_file_not_exist_or_is_empty
 
-if not os.path.isfile("./info.html"):
-    response = requests.get(url)
-    html = response.text
-    file = open("info.html", "w", encoding="utf-8")
+url_to_cinemas_in_perm = "https://perm.kinoafisha.info/cinema/"
+name_div_class_with_necessary_information = "cinemaList cinemaList-details outer-mobile"
+perm_cinemas_list = "perm_cinemas_list.html"
+
+if does_file_not_exist_or_is_empty(perm_cinemas_list):
+    response = requests.get(url_to_cinemas_in_perm)
+    raw_html = response.text
+    soup = BeautifulSoup(raw_html, "lxml")
+    html = str(soup.find(class_=name_div_class_with_necessary_information))
+    file = open(perm_cinemas_list, "w", encoding="utf-8")
     file.write(html)
     file.close()
 
-with open("info.html", encoding="utf-8") as file:
-    file = file.read()
+with open(perm_cinemas_list, encoding="utf-8") as current_cinemas_list:
+    current_cinemas_list = current_cinemas_list.read()
 
-soup = BeautifulSoup(file, "lxml")
+soup = BeautifulSoup(current_cinemas_list, "lxml")
 
 all_cinemas_hrefs = soup.find_all(class_="cinemaList_ref")
 all_cinemas_names = soup.find_all(class_="cinemaList_name")
 all_cinemas_addresses = soup.find_all(class_="cinemaList_addr")
+cinemas = []
 
-for item in all_cinemas_hrefs:
-	item_href = item.get("href")
-	print(item_href)
-for item in all_cinemas_names:
-	item_name = item.text
-	print(item_name)
-for item in all_cinemas_addresses:
-	item_address = item.text
-	print(item_address)
+for i in range(0, len(all_cinemas_hrefs)):
+    cinemas.append(Cinema(all_cinemas_hrefs[i].get("href"),
+                          all_cinemas_names[i].text,
+                          all_cinemas_addresses[i].text))
+
+for cinema in cinemas:
+    print(cinema.href)
+    print(cinema.name)
+    print(cinema.addresses)
+    print()
