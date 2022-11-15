62de825877a68e75e6a4ea4f45671d85efe87cc0
Nickita <fattahovnikita@mail.ru>
2022-11-15 11:59:37 +0500

    Parse films

--- web scraper/1) data extractor/engine.py
+++ web scraper/1) data extractor/engine.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import os.path
+import sys
+import os
 
 from cinema import Cinema
+sys.path.append("D:\\Miscellaneous\\PNRPU_TEAM_WEB_PROJECT")
+print(sys.path)
 from lib.file_checks import does_file_not_exist_or_is_empty
 
 url_to_cinemas_in_perm = "https://perm.kinoafisha.info/cinema/"
@@ -32,8 +38,48 @@ for i in range(0, len(all_cinemas_hrefs)):
                           all_cinemas_names[i].text,
                           all_cinemas_addresses[i].text))
 
+
 for cinema in cinemas:
     print(cinema.href)
     print(cinema.name)
     print(cinema.addresses)
     print()
+count = 0
+if count == 0:
+	req = requests.get(url=(cinema.href+"schedule/"))
+	src = req.text
+	with open(f"data/{count}_{cinema.name}.html","w",encoding="utf-8") as file:
+		file.write(src)
+	#with open(f"data/{count}_{cinema.name}.html","w+",encoding="utf-8") as file:
+	#	src = file.read()
+	soup = BeautifulSoup(src, "lxml")
+
+	all_films_names = soup.find_all(class_="showtimesMovie_name")
+	all_films_tags = soup.find_all(class_="showtimesMovie_categories")
+	all_films_details = soup.find_all(class_="showtimesMovie_details")
+	all_films_times = soup.find_all(class_="session_time")
+	all_films_prices = soup.find_all(class_="session_price")
+	all_films_pics  = soup.find_all(class_="picture_image") #хз
+
+	for item in all_films_names:
+		item_name=item.text
+		print(item_name)
+	for item in all_films_tags:
+		item_tags=item.text
+		print(item_tags)
+	for item in all_films_details:
+		item_details=item.text
+		print(item_details)
+	for item in all_films_times:
+		item_times=item.text
+		print(item_times)
+	for item in all_films_prices:
+		item_prices=item.text
+		print(item_prices)
+	for item in all_films_pics:
+		item_pics=item.get("href")
+		print(item_pics)
+	count += 1
+
+
+
