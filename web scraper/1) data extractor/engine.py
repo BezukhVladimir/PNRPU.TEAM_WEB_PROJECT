48c6428360231a69099eb334ecbf403f6c99bd19
Nickita <fattahovnikita@mail.ru>
2022-12-10 19:56:03 +0500

    add classes

--- web scraper/1) data extractor/engine.py
+++ web scraper/1) data extractor/engine.py
@@ -8,7 +8,8 @@ import os
 from cinema import Cinema
 sys.path.append("..\\..\\..\\PNRPU_TEAM_WEB_PROJECT")
 from lib.file_checks import does_file_not_exist_or_is_empty
-
+from lib.film import Film
+from lib.shows import Shows
 url_to_cinemas_in_perm = "https://perm.kinoafisha.info/cinema/"
 name_div_class_with_necessary_information = "cinemaList cinemaList-details outer-mobile"
 perm_cinemas_list = "perm_cinemas_list.html"
@@ -44,11 +45,15 @@ for cinema in cinemas:
     print(cinema.addresses)
     print()
 
+films = []
+shows = []
+timess = []
 count = 0
+
 for cinema in cinemas:
 	req = requests.get(url=(cinema.href+"schedule/"))
 	src = req.text
-	current_cinema = f"{count}_{cinema.name}.html"
+	current_cinema = f"data/{count}_{cinema.name}.html"
 
 	if does_file_not_exist_or_is_empty(current_cinema):
 		with open(f"data/{count}_{cinema.name}.html","w",encoding="utf-8") as file:
@@ -64,16 +69,41 @@ for cinema in cinemas:
 	
 	for item in all_films_names:
 		item_name=item.text
-		print(item_name)
+		#print(item_name)
 	for item in all_films_tags:
 		item_tags=item.text
-		print(item_tags)
+		#print(item_tags)
 	for item in all_films_details:
 		item_details=item.text
-		print(item_details)
+		det = item_details.split()
+		
+		#print(item_details)
+		print(det)
+	item_times = []
 	for item in all_films_times:
-		item_times=item.text
+		item_times = item.text
+		for obj in item_times:
+			obj.replace('\n', "")
+			#obj.strip()
+		item_times = item_times.replace('\n', " ")
+		arr = item_times.split()
+		for item in arr:
+			if item=="от" or item=="₽":
+				arr.remove(item)
+		item_times = arr
 		print(item_times)
-	
+	for i in range(0, len(all_films_names)):
+		films.append(Film(cinema.name,
+    		all_films_names[i].text,
+            all_films_tags[i].text,
+            all_films_details[i].text))
 	count += 1
 
+for film in films:
+    print(film.cinema)
+    print(film.name)
+    print(film.tags)
+    print(film.details)
+for show in shows:
+    print(show.time)
+    print(show.price)
