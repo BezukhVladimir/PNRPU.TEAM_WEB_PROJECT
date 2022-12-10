0b8c02b7dc05758d6082c6108edc983a5fed5a4b
Nickita <fattahovnikita@mail.ru>
2022-12-10 15:34:20 +0500

    added parsing by pages

--- web scraper/1) data extractor/engine.py
+++ web scraper/1) data extractor/engine.py
@@ -7,7 +7,6 @@ import os
 
 from cinema import Cinema
 sys.path.append("..\\..\\..\\PNRPU_TEAM_WEB_PROJECT")
-print(sys.path)
 from lib.file_checks import does_file_not_exist_or_is_empty
 
 url_to_cinemas_in_perm = "https://perm.kinoafisha.info/cinema/"
@@ -44,23 +43,25 @@ for cinema in cinemas:
     print(cinema.name)
     print(cinema.addresses)
     print()
+
 count = 0
-if count == 0:
+for cinema in cinemas:
 	req = requests.get(url=(cinema.href+"schedule/"))
 	src = req.text
-	with open(f"data/{count}_{cinema.name}.html","w",encoding="utf-8") as file:
-		file.write(src)
-	#with open(f"data/{count}_{cinema.name}.html","w+",encoding="utf-8") as file:
-	#	src = file.read()
+	current_cinema = f"{count}_{cinema.name}.html"
+
+	if does_file_not_exist_or_is_empty(current_cinema):
+		with open(f"data/{count}_{cinema.name}.html","w",encoding="utf-8") as file:
+			file.write(src)
+			
 	soup = BeautifulSoup(src, "lxml")
 
 	all_films_names = soup.find_all(class_="showtimesMovie_name")
+	#all_films_names = soup.find_all(class_="showtimesMovie_info")
 	all_films_tags = soup.find_all(class_="showtimesMovie_categories")
 	all_films_details = soup.find_all(class_="showtimesMovie_details")
-	all_films_times = soup.find_all(class_="session_time")
-	all_films_prices = soup.find_all(class_="session_price")
-	all_films_pics  = soup.find_all(class_="picture_image") #хз
-
+	all_films_times = soup.find_all(class_="showtimes_sessions")
+	
 	for item in all_films_names:
 		item_name=item.text
 		print(item_name)
@@ -73,13 +74,6 @@ if count == 0:
 	for item in all_films_times:
 		item_times=item.text
 		print(item_times)
-	for item in all_films_prices:
-		item_prices=item.text
-		print(item_prices)
-	for item in all_films_pics:
-		item_pics=item.get("href")
-		print(item_pics)
+	
 	count += 1
-
-
 
